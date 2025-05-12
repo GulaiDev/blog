c6a0ad69d7961a68a30733036d478a39f20fda58
laozefei <3302816060@qq.com>
2025-05-12 19:44:44 +0800

    feat:添加NavList组件

---
+++ frontend/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="app">
+    <NavList />
+    <main class="main-content">
+      <div class="container">
+        <router-view></router-view>
+      </div>
+    </main>
+  </div>
+</template>
+
+<script setup>
+import NavList from "./components/NavList.vue";
+</script>
+
+<style lang="scss" scoped>
+.app {
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+}
+
+.main-content {
+  flex: 1;
+  padding: $spacing-lg 0;
+}
+</style>
