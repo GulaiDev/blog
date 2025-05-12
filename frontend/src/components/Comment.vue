599ba06b7e6f7adac15f317fe84969aab1e82ff3
laozefei <3302816060@qq.com>
2025-05-12 19:17:20 +0800

    feat:添加评论组件

---
+++ frontend/src/components/Comment.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="comment-header">
+    <span class="comment-author">{{ props.comment.author }}</span>
+    <span class="comment-date">{{ formatDate(props.comment.created_at) }}</span>
+  </div>
+  <div class="comment-content">{{ props.comment.content }}</div>
+</template>
+
+<script setup>
+import { formatDate } from "@/utils/formatDate";
+const props = defineProps({
+  comment: {
+    type: Object,
+    required: true,
+  },
+});
+</script>
+
+<style scoped lang="scss">
+.comment-header {
+  margin-bottom: $spacing-xs;
+  font-size: $font-size-sm;
+
+  .comment-author {
+    font-weight: bold;
+    margin-right: $spacing-sm;
+  }
+
+  .comment-date {
+    color: $text-light;
+  }
+}
+
+.comment-content {
+  margin-bottom: $spacing-sm;
+  white-space: pre-wrap;
+}
+</style>
